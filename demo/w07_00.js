9347414d854281bcbf2c713ba3fb26de3d22007a
max920612 <max4961201@gmail.com>
2024-10-25 15:57:16 +0800

    add index.html as home page

--- demo/w07_00.js
+++ demo/w07_00.js
@@ -1,1 +1,16 @@
-alert('hello')
+const p = document.querySelector('.show-classdemo')
+console.log('show-classdemo', p)
+
+function showdemo(item) {
+    switch(item) {
+        case 'w02_intro_00':
+            p.innerHTML = `<iframe src='./w02-intro-00/index.html'  width="100%" height="100%" />`
+        break
+        case 'w03_card_00':
+            p.innerHTML = `<iframe src='./w03_card_00/index.html'  width="100%" height="100%" />`
+        break
+        case 'w06_blog_00':
+            p.innerHTML = `<iframe src='./w06_blog_00/index.html'  width="100%" height="100%" />`
+        break
+    }
+}
