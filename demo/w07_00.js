cba911b780abe94ababde2c4c3b004741c7ceb70
max920612 <max4961201@gmail.com>
2024-10-27 00:51:23 +0800

    add index.html as home page

--- demo/w07_00.js
+++ demo/w07_00.js
@@ -2,15 +2,15 @@ const p = document.querySelector('.show-classdemo')
 console.log('show-classdemo', p)
 
 function showDemo(item) {
-  switch (item) {
-    case 'w02-intro-02':
-      p.innerHTML = <iframe src='./w02-intro-00/index.html' width="100%" height="100%" />
-      break
-    case 'w03_card_02':
-      p.innerHTML = <iframe src='./W03_card_00/index.html' width="100%" height="100%" />
-      break
-    case 'w06_blog_02':
-      p.innerHTML = <iframe src='./W06_blog_00/index.html' width="100%" height="100%" />
-      break
-  }
+switch (item) {
+    case 'w02-intro-00':
+    p.innerHTML = <iframe src='./w02-intro-00/index.html' width="100%" height="100%" />
+    break
+    case 'w03_card_00':
+    p.innerHTML = <iframe src='./W03_card_00/index.html' width="100%" height="100%" />
+    break
+    case 'w06_blog_00':
+    p.innerHTML = <iframe src='./W06_blog_00/index.html' width="100%" height="100%" />
+    break
+    }
 }
