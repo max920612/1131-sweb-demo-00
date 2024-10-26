c0f85060afc8b5fcbecb5b270787c33540b1b513
max920612 <max4961201@gmail.com>
2024-10-27 01:02:38 +0800

    demo/w07_00.js

--- demo/w07_00.js
+++ demo/w07_00.js
@@ -1,16 +1,16 @@
-const p = document.querySelector('.show-classdemo')
-console.log('show-classdemo', p)
+const p = document.querySelector('.show-classdemo');
+console.log('show-classdemo', p);
 
 function showDemo(item) {
-switch (item) {
-    case 'w02-intro-00':
-    p.innerHTML = <iframe src='./w02-intro-00/index.html' width="100%" height="100%" />
-    break
-    case 'w03_card_00':
-    p.innerHTML = <iframe src='./W03_card_00/index.html' width="100%" height="100%" />
-    break
-    case 'w06_blog_00':
-    p.innerHTML = <iframe src='./W06_blog_00/index.html' width="100%" height="100%" />
-    break
+    switch (item) {
+        case 'w02-intro-00':
+            p.innerHTML = `<iframe src='./w02-intro-00/index.html' width="100%" height="100%"></iframe>`;
+            break;
+        case 'W03_card_00':
+            p.innerHTML = `<iframe src='./W03_card_00/index.html' width="100%" height="100%"></iframe>`;
+            break;
+        case 'W06_blog_00':
+            p.innerHTML = `<iframe src='./W06_blog_00/index.html' width="100%" height="100%"></iframe>`;
+            break;
     }
 }
