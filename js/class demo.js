13074277fc7cf6140bfa46576a8030ba87622f70
max920612 <max4961201@gmail.com>
2024-12-12 20:02:26 +0800

    W14-P2: Show class demo of w2, w3, w6 in both local and Vercel

---
+++ js/class demo.js
@@ -0,0 +1,17 @@
+const p = document.querySelector('.show-classdemo');
+console.log ('p', p )
+
+
+function showClassDemo(item){
+    switch(item){
+        case 'w02-intro-00':
+            p.innerHTML = `<iframe src='./demo/w02-intro-00/index.html' width='100%' height='100%' />`
+            break;
+        case 'W03_card_00':
+            p.innerHTML = `<iframe src='./demo/W03_card_00/' width='100%' height='100%' />`
+            break;
+        case 'W06_blog_00':
+            p.innerHTML = `<iframe src='./demo/W06_blog_00/' width='100%' height='100%' />`
+            break;
+    }
+}
