bfdd1ec97c54b361573606a341e96a8601fd02a8
max920612 <max4961201@gmail.com>
2025-01-02 21:37:59 +0800

    msad

--- js/class demo.js
+++ js/class demo.js
@@ -1,17 +1,22 @@
-const p = document.querySelector('.show-classdemo');
-console.log ('p', p )
+const p = document.querySelector('.show-classdemo')
+console.log('p', p)
 
-
-function showClassDemo(item){
-    switch(item){
-        case 'w02-intro-00':
-            p.innerHTML = `<iframe src='./demo/w02-intro-00/index.html' width='100%' height='100%' />`
-            break;
-        case 'W03_card_00':
-            p.innerHTML = `<iframe src='./demo/W03_card_00/' width='100%' height='100%' />`
-            break;
-        case 'W06_blog_00':
-            p.innerHTML = `<iframe src='./demo/W06_blog_00/' width='100%' height='100%' />`
-            break;
-    }
+function showClassDemo(item) {
+  switch (item) {
+    case 'w02-intro-00':
+      p.innerHTML = `<iframe src='./demo/w02-intro-00/index.html' width='100%' height='100%' />`
+      break
+    case 'W03_card_00':
+      p.innerHTML = `<iframe src='./demo/W03_card_00/' width='100%' height='100%' />`
+      break
+    case 'W06_blog_00':
+      p.innerHTML = `<iframe src='./demo/W06_blog_00/' width='100%' height='100%' />`
+      break
+    case 'W11_navbar_00':
+      p.innerHTML = `<iframe src='./demo/W11_navbar_00/' width='100%' height='100%' />`
+      break
+    case 'w13_multipage_00':
+      p.innerHTML = `<iframe src='./demo/w13_multipage_00/index.html' width='100%' height='100%' />`
+      break
+  }
 }
